1ce0aea5b0857783bf1fb31a738ab2e466892d60
akani28 <nietocevallos61@gmail.com>
2023-07-10 03:38:56 -0500

    Consumimos la data desde una Api.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("funciona el mainJS")
+
+async function getProducts(){
+try {
+    const data = await fetch("https://ecommercebackend.fundamentos-29.repl.co/");
+    const res = await data.json();
+    return res;
+} catch (error) {
+    console.log(error);
+    
+}
+
+}
+async function main(){
+    const res = await getProducts();
+    console.log(res);
+}
+main();
